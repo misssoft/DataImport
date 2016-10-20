c630bd996b1f6d7b757abc1dee95764335dd660c
Fan Yang <fan.yang.turner@live.co.uk>
2016-10-20 22:39:40 +0100

    Add main function

---
+++ Importer/Importer/ImportDataFunction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Importer
+{
+    public static class ImportDataFunction
+    {
+        public static void ImportData(
+            Func<FileInfo> GetZipFile,
+            Func<FileInfo, IEnumerable<FileInfo>> UnzipFile,
+            Func<IEnumerable<FileInfo>, IEnumerable<PeopleData>> Transform
+            )
+        {
+            var file = GetZipFile();
+            var extractedfiles = UnzipFile(file);
+            var peopleList = Transform(extractedfiles)
+        }
+    }
+
+    public class PeopleData{}
+}
